38587b7e37cf0de5e051a6e5acd2b3b3b4e1a05b
zh <travel_zh@163.com>
2024-11-13 01:55:53 +0800

    add file

--- repository/net/src/main/java/com/custom/net/PaymentApi.kt
+++ repository/net/src/main/java/com/custom/net/PaymentApi.kt
@@ -4,7 +4,7 @@ import com.custom.net.base.BaseNetworkApi
 import okhttp3.Interceptor
 
 class PaymentApi : BaseNetworkApi("") {
-    override fun getInterceptor(): Interceptor? {
+    override fun getInterceptor(): Interceptor {
         return Interceptor { chain ->
             val time = System.currentTimeMillis().toString()
             var newBuilder = chain.request().newBuilder()
